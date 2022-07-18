b5b7632441ff2cc9c5ec57e035b2dbc0e07333d6
Carlos Botelho Neto <cbneto@gmail.com>
2022-07-18 10:56:39 -0300

    Envio do deploy

---
+++ deploy.php
@@ -0,0 +1,41 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'my_project');
+
+// Project repository
+set('repository', 'https://github.com/carlosbotelhoneto/plantaoativo.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('ubuntu@144.22.128.216')
+    ->identityFile('/Users/carlosbotelhoneto/Sites/navi/cloud/ssh-key-2022-06-14.key')
+    ->set('deploy_path', '/var/www/cinforms');
+    
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
